1e507bfa50e6371ab291f49e497eee34f0017b24
Livio Bieri <livio@livio.li>
2013-12-10 22:19:33 +0100

    More is less: Reduced informations provided by Rsync during syncing to what's useful.

--- SyncMe/rSyncMeNow.rb
+++ SyncMe/rSyncMeNow.rb
@@ -1,55 +1,50 @@
 #! /usr/bin/ruby
 
 # Get your study documents automatically from active directory
-#
-# @author Marius Küng
-# @version 0.1 (2013-11-21)
+# @author Marius Küng and Livio Bieri
 
-# we are the cool kids so we need json here
 require 'json'
 require 'optparse'
+require 'require_relative'
 
-
-options = {
-    # defaulting to subjects.json and settings.json assuming 
-    # they are located under the same path as the script itself
-    :subjectsFilePath => File.join(File.dirname(__FILE__),'subjects.json'),
-    :settingsFilePath => File.join(File.dirname(__FILE__),'settings.json')
-}
+# make sure that keys which have not been 
+# set in the hash map return nil so we can use ||
+options = Hash.new(nil) 
 
 OptionParser.new do |opts|
-    opts.banner = "Usage: rSyncMeNow.rb [options]"
-    opts.on("-s", "--subjects", "Absolute path to a subject file in json format") do |s|
-        options[:subjectsFilePath] = s
+    opts.banner = 'Usage: rSyncMeNow.rb [optional parameters]'
+    opts.on('-s', '--subjects [OPT]', 'Absolute path to a subject file.') do |subjectUserPath|
+        options[:subjectsFilePath] = subjectUserPath
     end
-    opts.on("-c", "--settings", "Absolute path to a setting file in json format") do |c|
-        options[:subjectsFilePath] = c
+    opts.on('-c', '--settings [OPT]', 'Absolute path to a setting file.') do |settingUserPath|
+        options[:settingsFilePath] = settingUserPath
     end
+
+    # setting default files if necessary
+    options[:subjectsFilePath] = options[:subjectsFilePath] || './subjects.json'
+    options[:settingsFilePath] = options[:settingsFilePath] || './settings.json'
+
 end.parse!
 
 begin 
     subjects = JSON.parse(File.read(options[:subjectsFilePath]))
     settings = JSON.parse(File.read(options[:settingsFilePath]))
 rescue
-    puts " => Ooops. Error while parsing files. :-/ \n" +
-         "    Verify your subjects and settings file. \n" +
-         "    Make sure they are existing and valid. Aborting now. :-("
+    puts 'Ooops! Error while parsing files. => Make sure specified files are valid. :-('
     exit
 end
 
-# Check if Active Directory is around
 if not File.directory?(settings["SOURCE_PATH"]) then
-    puts "Connection was not found to active directory. Hell no! Aborting now. :-("
+    puts 'Ooops. Connection was not found to active directory. => Connect active directory first. :-('
     exit
 end
 
 subjects["subjects"].each do |subject|
 
     subjectMirrorFolder = "%s/%s/%s/" % [settings["USER_PATH"], subject['name'], settings["MIRROR_PATH"]]
-
     puts " => %s [%s]" % [subject["name"], subjectMirrorFolder]
 
-    # if target directory doesn't exitst
+    # if target directory doesn't exist create it
     if !File.directory?(File.expand_path(subjectMirrorFolder)) then
         system("mkdir -p %s" % subjectMirrorFolder)
     end
@@ -66,6 +61,5 @@ subjects["subjects"].each do |subject|
         subject['name'],
         settings["MIRROR_PATH"]
     ]
-    system( rsync ) # run a shell command: system ("<command>")
-
+    system( rsync )
 end
