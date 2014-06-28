3cd1b7503d7cdb32c8e8428abff05fe241b9c236
Livio Bieri <livio@livio.li>
2014-06-28 15:40:57 +0200

    Subjects unit test.

--- Core/Turbo Octo Hipster/Turbo Octo HipsterTests/SubjectTest.swift
+++ Core/Turbo Octo Hipster/Turbo Octo HipsterTests/SubjectTest.swift
@@ -7,11 +7,11 @@
 //
 
 import XCTest
-import turbooctohipster
+import Turbo_Octo_Hipster
 
 class SubjectTest: XCTestCase {
     
-    var objectUnderTest: SubjectProtocol
+    var objectUnderTest: SubjectProtocol! = nil
 
     override func setUp() {
         super.setUp()
@@ -22,14 +22,9 @@ class SubjectTest: XCTestCase {
     }
 
     func testInit() {
+        objectUnderTest = Subject(abbreviation: "mada", directory: "~/Desktop/")
         
+        XCTAssertEqual(objectUnderTest.abbreviation, "mada")
+        XCTAssertEqual(objectUnderTest.directory, "~/Desktop/")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
